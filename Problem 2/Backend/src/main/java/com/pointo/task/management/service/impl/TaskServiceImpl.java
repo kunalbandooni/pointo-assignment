c231b6f8e87f3bf460874ea680472f9c90f91c66
kunalbandooni <kbandooni1@gmail.com>
2025-06-05 13:52:27 +0530

    BE: Added global exception handler

--- Problem 2/Backend/src/main/java/com/pointo/task/management/service/impl/TaskServiceImpl.java
+++ Problem 2/Backend/src/main/java/com/pointo/task/management/service/impl/TaskServiceImpl.java
@@ -2,6 +2,7 @@ package com.pointo.task.management.service.impl;
 
 import com.pointo.task.management.dto.TaskRequestDTO;
 import com.pointo.task.management.entity.Task;
+import com.pointo.task.management.exception.ValidationException;
 import com.pointo.task.management.repo.TaskRepository;
 import com.pointo.task.management.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,7 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Task getById(Long id) {
-        // TODO: handle null, throw exception not found with proper response
-        return taskRepository.findById(id).orElse(null);
+        return taskRepository.findById(id).orElseThrow(() -> new ValidationException("Task not found"));
     }
 
     @Override
@@ -40,7 +40,7 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public Task updateTask(Long id, TaskRequestDTO dto) {
         Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Task not found"));
+                .orElseThrow(() -> new ValidationException("Task not found"));
 
         task.setTitle(dto.getTitle());
         task.setDescription(dto.getDescription());
@@ -54,7 +54,7 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public Task updateTaskStatus(Long id, Task.Status status) {
         Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Task not found"));
+                .orElseThrow(() -> new ValidationException("Task not found"));
 
         task.setStatus(status);
         return taskRepository.save(task);
@@ -63,7 +63,7 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void softDeleteTask(Long id) {
         Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Task not found"));
+                .orElseThrow(() -> new ValidationException("Task not found"));
         task.setDeleted(true);
         taskRepository.save(task);
     }
